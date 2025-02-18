b38457b5cb80977d5557c964a28baec6abcc5fb1
Adrian Grieger <hurznburzn@gmail.com>
2025-02-19 00:00:50 +0100

    enhanced websocket server error handling and logging

--- websocket_server.py
+++ websocket_server.py
@@ -3,18 +3,20 @@ import vosk
 import asyncio
 import websockets
 
-# Vosk-Modell laden
+# Choose language model
 MODEL_PATH = "vosk_model/vosk-model-en-us-0.22"
 model = vosk.Model(MODEL_PATH)
 
 async def recognize(websocket, path=""):
     recognizer = vosk.KaldiRecognizer(model, 16000)
     async for message in websocket:
+        print("Received audio data of length:", len(message))
         if recognizer.AcceptWaveform(message):
             text = json.loads(recognizer.Result())["text"]
             await websocket.send(json.dumps({"recognized_text": text}))
         else:
-            await websocket.send(json.dumps({"recognized_text": ""}))
+            print("Failed to recognize audio")
+            await websocket.send(json.dumps({"Error": "Failed to recognize audio"}))
 
 async def main():
     async with websockets.serve(recognize, "0.0.0.0", 5000):
