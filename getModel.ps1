71ac3b9a83fc2af3b2e1be7973fb462c3233679a
Adrian Grieger <hurznburzn@gmail.com>
2025-02-19 00:00:36 +0100

    added german model

--- getModel.ps1
+++ getModel.ps1
@@ -1,5 +1,6 @@
-pip install vosk flask sounddevice
 mkdir vosk_model
 cd vosk_model
 Invoke-WebRequest -Uri "https://alphacephei.com/vosk/models/vosk-model-en-us-0.22.zip" -OutFile "vosk-model-en-us-0.22.zip"
+Invoke-WebRequest -Uri "https://alphacephei.com/vosk/models/vosk-model-de-0.21.zip" -OutFile "vosk-model-de-0.21.zip"
 Expand-Archive -Path "vosk-model-en-us-0.22.zip" -DestinationPath .
+Expand-Archive -Path "vosk-model-de-0.21.zip" -DestinationPath .
